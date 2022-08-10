25344dc3ea3e2cf05a9075c80c4dc8a60a8b6c09
Anibal Hugo <ani2bal@yahoo.com>
2022-08-10 13:48:44 -0400

    adding bootstrap grid and images

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,9 @@ function error404 () {
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p>Oops, sorry, we can't find this page!</p>
+            <img src="/images/error-ice-cream.jpg" alt="Spilled ice cream" />
+            <p>Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/s/photos/error?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  </p>
         </main>
     </Def>
   )
