6fefcb06aebb3750d2238510c2ef7c406cfd8450
Anibal Hugo <ani2bal@yahoo.com>
2022-08-15 21:08:26 -0400

    added icon to delete button

--- views/default.jsx
+++ views/default.jsx
@@ -6,6 +6,7 @@ function Def (html) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
@@ -22,9 +23,9 @@ function Def (html) {
                     </li>
                 </ul>
             </nav>
-
                 {html.children}
             </body>
+            <footer>Website built by Hugo</footer>
         </html>
     )
   }
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,16 +6,18 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <img src= {data.place.pic} alt="Image of place" />
             <h2>Rating</h2>
             <p>Currently not rated!</p>
             <h2>Comments</h2>
             <p>No comments yet!</p>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-              Edit
-            </a>     
+            Edit 
+            </a>   
+            <br /> 
             <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-              <button type="submit" className="btn btn-danger">
-                Delete
+              <button type="submit" className="btn btn-danger"><i class="fa fa-trash"> Delete </i>
+                
               </button>
             </form> 
           </main>
