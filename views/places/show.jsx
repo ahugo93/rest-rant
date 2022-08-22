82d1131468d7a4ac0c0d1f9c34f0386d075ab30d
Anibal Hugo <ani2bal@yahoo.com>
2022-08-22 12:23:56 -0400

    applied changes to show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,12 +3,29 @@ const Def = require('../default')
 
 function show (data) {
     return (
-        <Def>
-          <main>
-            <h1>{ data.place.name }</h1>
-            <img src= {data.place.pic} alt="Image of place" />
+      <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
             <h2>Rating</h2>
             <p>Currently not rated!</p>
+            <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+        </div>
+        </div>
             <h2>Comments</h2>
             <p>No comments yet!</p>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
@@ -21,7 +38,7 @@ function show (data) {
             </form> 
           </main>
         </Def>
-    )
-}
+        )}
 
 module.exports = show
+
