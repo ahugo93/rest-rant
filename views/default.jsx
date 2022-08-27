6060484c71b7d17cf2f47db39c7496dee98ec42b
Anibal Hugo <ani2bal@yahoo.com>
2022-08-26 20:49:40 -0400

    added stars

--- views/default.jsx
+++ views/default.jsx
@@ -4,8 +4,8 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
+                <title>Rest-RANT</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,26 @@ function show (data) {
         No comments yet!
       </h3>
     )
+    let rating = (
+      <h3 className="inactive">
+        Not yet rated
+      </h3>
+    )
+    if (data.place.comments.length) {
+      let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += ' ⭐️ '
+      }
+      rating = (
+        <h3>
+          {stars} stars
+        </h3>
+      )
+    }
     if (data.place.comments.length) {
       comments = data.place.comments.map(c => {
         return (
@@ -36,9 +56,7 @@ function show (data) {
                 <h2>
                   Rating
                 </h2>
-                <h3 className="inactive">
-                  Not Rated
-                </h3>
+                { rating }
                 <br />
                 <h2>
                   Description
@@ -95,6 +113,7 @@ function show (data) {
     )
 }
 module.exports = show
+
 
 
 
